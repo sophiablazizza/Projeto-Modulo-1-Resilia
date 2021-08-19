3d5cfb319f3c025a72b9bfd6e27199e0a75b5bb2
Sophia Blazizza <sblazizza@gmail.com>
2021-08-19 02:34:37 -0300

    alterações no js

--- personagem1-emo/fase2mcr.js
+++ personagem1-emo/fase2mcr.js
@@ -5,7 +5,7 @@ function escolha () {
 
         if (caminho == 1) {
             alert ("Não precisamos de almas revoltadas! GAME OVER.")
-            return location.href = "./index.html"
+            return location.href = "../index.html"
         }
         else if (caminho == 2) {
             return location.href = "./fase3mcr.html"
--- personagem2-otaku/fase1.js
+++ personagem2-otaku/fase1.js
@@ -5,7 +5,7 @@ function escolha() {
 
         if (caminho == 1) {
             alert ("É BOLACHAAAAAAAAAAAAAAA. Game over.")
-            return location.href = "./index.html"
+            return location.href = "../index.html"
         }
         else if (caminho == 2) {
             return location.href = "./fase2.html"
